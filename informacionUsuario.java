0846c01d1438e345e18723fd078e5dd4bd510ea3
KarenGil1 <82838111+KarenGil1@users.noreply.github.com>
2022-01-22 19:52:11 -0700

    Add files via upload

---
+++ informacionUsuario.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package healthylife3;
+
+/**
+ *
+ * @author vane_
+ */
+public class informacionUsuario extends infoo {
+}
+    /*String nombreUsuario, edad, estatura, peso, dietaRecomendada;
+
+    public informacionUsuario(String nombreUsuario, String edad, String estatura, String peso, String dietaRecomendada) {
+        super(nombreUsuario, edad, estatura, peso, dietaRecomendada);
+    }
+
+    
+    
+    public boolean nuevoUsuario(int nombreUsuario, int edad, int estatura, int peso, String dietaRecomendada){
+        try{
+            while(opt != 0){
+                if()
+            }
+        }
+    }
+}*/
